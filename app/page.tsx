9ea1c089d7df3962e53bda1714dd8d74c2d7be1c
danielmdavis <danis1911@gmail.com>
2023-03-16 13:46:23 -0400

    pulling from firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,32 @@
+import React from 'react'
+import firebase from 'firebase/compat/app'
+import 'firebase/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
 import InnerHome from './statefulChild'
 
 export default function Home() {
+
+  // firebase.initializeApp({
+  //   apiKey: "AIzaSyB-lNG6danS5wodEmWpVEmTFbcesdx3qfE",
+  //   authDomain: "microchat-30c83.firebaseapp.com",
+  //   projectId: "microchat-30c83",
+  //   storageBucket: "microchat-30c83.appspot.com",
+  //   messagingSenderId: "198227390543",
+  //   appId: "1:198227390543:web:f778007893bed6bf2b3a3a",
+  //   measurementId: "G-G6QEH32V7G"
+  // })
+
+  // const firestore = firebase.firestore()
+
+  // const getAll = () => {
+  //   const messageRef = firestore.collection('messages')
+  //   const query = messageRef.get()
+  //   const [allMessages]: object[] = useCollectionData(query)
+  //   console.log(allMessages)
+  // }
+  // getAll()
+
 
   return (
     <main>
--- app/statefulChild.tsx
+++ app/statefulChild.tsx
@@ -3,12 +3,59 @@ import { useState, useEffect } from 'react'
 import Message from './messageComponent'
 import messageJson from './messages.json'
 
+
+// import firebase from 'firebase/compat/app'
+// import { getFirestore, query, collection } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app'
+import { getFirestore, collection, getDocs, doc, setDoc } from 'firebase/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+
 export default function InnerHome() {
 
   let [messages, setMessages] = useState([])
   let [inputText, setInputText] = useState('')
+  let [myIp, setMyIp] = useState('')
 
-  useEffect(() => { setMessages(messageJson) })
+
+  //
+  const firebaseApp = initializeApp({
+    apiKey: "AIzaSyB-lNG6danS5wodEmWpVEmTFbcesdx3qfE",
+    authDomain: "microchat-30c83.firebaseapp.com",
+    projectId: "microchat-30c83",
+    storageBucket: "microchat-30c83.appspot.com",
+    messagingSenderId: "198227390543",
+    appId: "1:198227390543:web:f778007893bed6bf2b3a3a",
+    measurementId: "G-G6QEH32V7G"
+  })
+
+  const db = getFirestore(firebaseApp)
+  // const messagesRef = collection(db, 'messages')
+
+
+  const getAll = async () => {
+    const messagesCollection = collection(db, 'messages')
+    const query = await getDocs(messagesCollection)
+    const messagesList = query.docs.map(doc => doc.data());
+    // const [allMessages]: object[] = useCollectionData(messagesCollection)
+    console.log(messagesList)
+  }
+  getAll()
+//
+
+  useEffect(() => { 
+    setMessages(messageJson) 
+    getMyIp()
+  })
+
+  const getMyIp = () => {
+    fetch('https://api64.ipify.org?format=json', {
+  })
+    .then(req => req.json())
+    .then(res => {
+      setMyIp(res.ip)
+    })
+  }
 
   let id = 0
   const mappedMessages = messages.map((item) => {
@@ -24,7 +71,7 @@ export default function InnerHome() {
 
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      messages.push({ 'name': 'ip', 'message': inputText })
+      messages.push({ 'name': myIp, 'message': inputText, 'time': new Date() })
       setMessages(messages)
       setInputText('')
     }
@@ -33,6 +80,7 @@ export default function InnerHome() {
   return (
     <div className='app'>
       {mappedMessages}
+      <div className='ip'>{myIp}</div>
       <input className='textbox' onChange={(event) => {setInputText(event.target.value)}} value={inputText} onKeyDown={handleKeyDown} />
     </div>
   )
